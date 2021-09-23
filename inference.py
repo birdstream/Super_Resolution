e40d73788d438c45fa57bc733cf8278f39cbaf42
Joakim Fogelström <birdstream@hotmail.com>
2021-09-23 22:59:19 +0200

    Updated model. Instead of edge-padding blocks from 208x208 to 224x224, use a rolling-window strategy instead (lessens re-stitching artifacts)

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 #import argparse
-from skimage.util import view_as_blocks
+from skimage.util import view_as_windows
 import io
 from fastapi import FastAPI, Response, File, Form
 from fastapi.staticfiles import StaticFiles
@@ -33,27 +33,25 @@ async def predict(bild: bytes = File(...), training: bool = Form(False)):
     img = img.resize((W, H))
     
     # Since the model only takes the image in blocks of 208x208, we need to pad the image to be evenly divideable with this size
-    W_1 = np.ceil(W / 208) * 208
-    H_1 = np.ceil(H / 208) * 208
-    padded_img = Image.new('RGB', (np.uint16(W_1), np.uint16(H_1)), 0)
-    padded_img.paste(img)
+    #W_1 = np.ceil(W / 208) * 208
+    #H_1 = np.ceil(H / 208) * 208
+    #padded_img = Image.new('RGB', (np.uint16(W_1), np.uint16(H_1)), 0)
+    #padded_img.paste(img)
     
     # Convert the padded image to an array
-    img_array = tf.keras.preprocessing.image.img_to_array(padded_img)
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = np.pad(img_array, ((0,224), (0,224), (0, 0)))
     
     # Now slice it to 208x208 blocks
-    img_array = view_as_blocks(img_array, (208, 208, 3))
+    img_array = view_as_windows(img_array, (224, 224, 3), step=208)
     
     # Pad each block with edge pixels to 224x224 (the actual input size for the model)
-    img_array = np.pad(img_array, ((0, 0), (0, 0), (0, 0), (8, 8), (8, 8), (0, 0)), mode = 'edge')
     x = img_array.shape
     
     
     # Get the number of blocks for the prediction loop
     block_rows, block_cols = x[:2]
-    
-    # Value to divide the pixel values with so that they are between 0 and 1
-    norm = 255 
+     
     
     # Create an empty array where we put the predicted blocks
     img_array_out = np.zeros((block_rows * 208, block_cols * 208, 3), dtype = np.float32)
